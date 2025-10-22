0bb25e59f10103b4d7dc3412e20f9f317adcfc6d
t343s <t343s@PC-01>
2025-10-22 22:14:27 +0300

    +

--- ConsoleCs_TaskManagerLogic/Infrastructure/Services/JwtService.cs
+++ ConsoleCs_TaskManagerLogic/Infrastructure/Services/JwtService.cs
@@ -1,9 +1,13 @@
-﻿namespace ConsoleCs_TaskManagerLogic.Infrastructure.Services
+﻿using ConsoleCs_TaskManagerLogic.Model.DataType;
+using Microsoft.Extensions.Options;
+
+namespace ConsoleCs_TaskManagerLogic.Infrastructure.Services
 {
-    public class JwtService : ITokenService
+    public class JwtService(IOptions<AuthSettings> options) : ITokenService
     {
         public string GenerateToken(string login)
         {
+            var token = 
             throw new NotImplementedException();
         }
     }
